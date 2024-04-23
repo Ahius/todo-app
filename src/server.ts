7ac6c70dcd2b6d1051321c6b1d78f6f1727a63f6
Doan Thanh Hieu <hieudoan280102@gmail.com>
2024-04-23 20:56:21 +0700

    create, login user

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import connectToDatabase from './db';
+import userRoutes from '../routes/user.routes';
 
 
 const aplication = express();
+aplication.use(express.json());
 const PORT = 1337;
 
 connectToDatabase();
@@ -11,6 +13,10 @@ connectToDatabase();
 aplication.get('/ping', (request: Request, respone: Response) => {
     respone.send('Pong')
 });
+
+aplication.use("/user", userRoutes);
+
+
 
 aplication.listen(PORT, () => {
     console.log('Server up and running');
